9f4904f82e04ea5dc86d46308bf9882bef8b71d7
David Pfau <dbp2112@elk.cc.columbia.edu>
2014-07-01 17:33:51 -0400

    Update for Tue Jul  1 17:33:51 EDT 2014

--- eos.sh
+++ eos.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+# Last Updated Tue Jul  1 17:33:51 EDT 2014
 
-cat eos.sh
-git commit -a -m "test again"
+foo=$(sed "2 s/Last Updated.*/Last Updated `date`/" <eos.sh)
+echo "$foo" > eos.sh
+
+git commit -a -m "Update for `date`"
 git push
