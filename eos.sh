5c5b1df1ae4edc8f11cf2c6eebe54fe6a025877a
David Pfau <dbp2112@elk.cc.columbia.edu>
2014-07-01 17:35:22 -0400

    Update for Tue Jul  1 17:35:22 EDT 2014

--- eos.sh
+++ eos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Last Updated Tue Jul  1 17:34:01 EDT 2014
+# Last Updated Tue Jul  1 17:35:22 EDT 2014
 
 foo=$(sed "2 s/Last Updated.*/Last Updated `date`/" <eos.sh)
 echo "$foo" > eos.sh
