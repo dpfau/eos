362dc65df9f3acdbe6d26038a87827ff0b6c500b
David Pfau <david.pfau@gmail.com>
2014-07-03 23:54:50 -0400

    Changed script to cd into folder, no matter where it's called from

--- eos.sh
+++ eos.sh
@@ -9,8 +9,10 @@
 # Last Updated Thu Jul  3 23:33:11 EDT 2014
 
 folder='/home/ec2-user/eos'
-foo=$(sed "9 s/Last Updated.*/Last Updated `date`/" <$folder/eos.sh)
-echo "$foo" > $folder/eos.sh
+cd $folder
+
+foo=$(sed "9 s/Last Updated.*/Last Updated `date`/" <eos.sh)
+echo "$foo" > eos.sh
 
 crontab -r # Kill current cron jobs
 foo=$(expr $(date +%w) + 1) # Day of the week + 1 (ok since +%w returns 0-6, but cron recognizes 0-7), used to insure we don't execute twice in one day.
