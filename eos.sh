d124c628aadec741ee7f4b285270dd272791d7b3
David Pfau <david.pfau@gmail.com>
2014-11-21 06:54:01 +0000

    Update for Fri Nov 21 06:54:01 UTC 2014

--- eos.sh
+++ eos.sh
@@ -6,7 +6,7 @@
 #
 # David Pfau, waiting for a connecting flight in Fiumicino, 1 July 2014
 #
-# Last Updated Thu Nov 20 06:52:01 UTC 2014
+# Last Updated Fri Nov 21 06:54:01 UTC 2014
 
 folder='/home/ec2-user/eos'
 cd $folder
